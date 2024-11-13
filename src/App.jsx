2c955a6ef8767bab4ee87734c7e1a2c2e17a3a1e
Zairaxx <brandontsegai@gmail.com>
2024-11-13 10:47:28 +0100

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,22 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import SmallComponent from './components/SmallComponent';
 import './App.css'
 
 function App() {
+  let [show,setShow] = useState(false);
 
-  let [todos,setTodos] = useState([
-    {
-        title:"Todo #1",
-        desc:"Städa",
-        completed:false
-    },
-    {
-      title:"Todo #2",
-      desc:"Städa",
-      completed:false
-  },
-  {
-    title:"Todo #3",
-    desc:"Städa",
-    completed:true
-},
-{
-  title:"Todo #4",
-  desc:"Städa",
-  completed:false
-},
-{
-  title:"Todo #5",
-  desc:"Städa",
-  completed:true
-}
-  ])
-  let [title, setTitle] = useState('');
-  let [description, setDescription] = useState('')
 
-  const addTodo = () => {
-    
-    let newTodo = {
-      title:title,
-      desc:description,
-      completed:false
-    }
+  useEffect(() => {
+    console.log("När applikationen startas")
+}, [])
 
-    let updatedTodos = [...todos, newTodo]
-    // updatedTodos.push(newTodo)
-    setTodos(updatedTodos)
 
-  } 
-
-  const deleteTodo = (index) => {
-    //Få bort den klickade todon från listan.
-    
-    let updatedTodos = todos.filter((todo,i) => {return i !== index})
-    setTodos(updatedTodos);
-  }
-
-  const completeTodo = (index) => {
-
-    let updatedTodos = [...todos];
-    updatedTodos[index].completed = true;
-    setTodos(updatedTodos);
-  }
 
   return (
     <>
-    <h1>Todo app in React</h1>
-    <input type="text" placeholder="Titel" onChange={(event) => setTitle(event.target.value)}/>
-    <input type="text" placeholder="Beskrivning" onChange={(event) => setDescription(event.target.value)}/>
-    <button onClick={addTodo}>Lägg till</button>
-    {/* Todo list */}
-    <ul>
-      {todos.map((todo,i) => <li key={i}>
-        <h2 className={todo.completed ? 'completed' : ""}>{todo.title}
-          <button onClick={() => deleteTodo(i)}>X</button>
-          <button onClick={() => completeTodo(i)}>Complete</button>
-        </h2>
-        <p>{!todo.completed && todo.desc}</p>
-        </li>)}
-    </ul>
+    {show && <SmallComponent/>}
+    <h1>Component Lifecycle - useEffect</h1>
+    <button onClick={() => setShow(!show)}>Toggle</button>
     </>
   )
 }
